2e9243d3cdded2274f17e4263dd07b57df8cb7d3
Verondrummond171 <56696878+Verondrummond171@users.noreply.github.com>
2019-10-17 14:13:00 -0500

    Update Trainer.py

--- Trainer.py
+++ Trainer.py
@@ -1,1 +1,343 @@
+import kivy
+import socket
 
+def detect():
+Search.device freefire
+def install ():
+install= mod.game
+class mod.game():
+//when installing pull requests=hack.diamond
+Validate.freefire
+def give():
+give.self players email(coin,diamonds,Ruby,headshot)
+self.find player
+send.file player
+send.install mod.game
+def reset.freefire():
+self.output display (click here reset game)
+def cleardata():
+collect.self data.save
+def coin():
+for C in coin(validate.find player=1000000)
+validate.self unlimited coin
+send.coin=player
+
+
+
+
+gg.toast('FreeFire Script v2.9 by SmartCheat')
+print('FreeFire Script By SmartCheat') 
+
+--Game guardian detection
+if gg.isVisible(true) then 
+   gg.setVisible(false) 
+   end 
+
+::start::
+menu = gg.choice({'1.Remove AntiHack','2.HackMenu','3.SecondaryMenu','4.ThirdMenu','5.FourthMenu'},nil,'SelectHack')
+if menu == 1 then goto bps end
+if menu == 2 then goto SmartCheater end
+if menu == 3 then goto SecondaryMenu end
+if menu == 4 then goto ThirdMenu end
+if menu == 5 then goto FourthMenu end
+if menu == nil then gg.alert('Happy Cheating') end goto creator
+
+::SmartCheater::
+menu = gg.choice({'1.Giant Body','2.Giant Only On Car[Activate On Plane]','3.Suicide','4.Antenna Female ','5.Antenna Male','6.God Mode[Activate In Game]','7.No Fall','8.Underground','9.The Flash Of Crocodile [Activate in Lobby]','10.Eagle Eye','11.WallHack (Nivek)'},last,'List of HackMenu')
+if menu == 1 then goto GiantBody end
+if menu == 2 then goto BC end
+if menu == 3 then goto InstantDeath end
+if menu == 4 then goto Antenna end
+if menu == 5 then goto antenna2 end
+if menu == 6 then goto god end
+if menu == 7 then goto on end
+if menu == 8 then goto Underground end
+if menu == 9 then goto fc end
+if menu == 10 then goto FLY end
+if menu == 11 then goto WallHack end
+if menu == nil then gg.alert('Happy Cheating') end goto start
+
+print('CancelScript')
+
+::SecondaryMenu::
+menu = gg.choice({'1.BLUE MAP','2.GOLDEN MAP','3.GREEN MAP','4.PURPLE MAP','5.RED MAP','6.LIGHT BLUE MAP','7.YELLOW MAP'},nil,'WEAPONS, MAP [SET OF COLORS]')
+if menu == 1 then goto BlueMap end
+if menu == 2 then goto GoldenMap end
+if menu == 3 then goto GreenMap end
+if menu == 4 then goto PurpleMap end
+if menu == 5 then goto RedMap end
+if menu == 6 then goto LightBlueMap end
+if menu == 7 then goto YellowMap end
+if menu == nil then gg.alert('Thanks To Nivek')end goto start
+
+print('CancelScript')
+
+::BlueMap::
+gg.searchNumber('0.69999998808F;0.61197602749;0.58263474703', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber('0.69999998808', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(10000)
+gg.editAll('999', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.clearResults()
+gg.toast('Hack BLUE MAP Done')
+goto SecondaryMenu
+
+::GoldenMap::
+gg.searchNumber('0.69999998808F;0.61197602749;0.58263474703', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber('0.69999998808', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(10000)
+gg.editAll('-999', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.clearResults()
+gg.toast('Hack GOLDEN MAP Done')
+goto SecondaryMenu
+
+::GreenMap::
+gg.searchNumber('0.61197602749;0.58263474703;0.69999998808', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber('0.61197602749', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(10000)
+gg.editAll('999', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.clearResults()
+gg.toast('Hack GREEN MAP Done')
+goto SecondaryMenu
+
+::PurpleMap::
+gg.searchNumber('0.61197602749;0.58263474703;0.69999998808', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(10000)
+gg.editAll('-999', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.clearResults()
+gg.toast('Hack PURPLE MAP Done')
+goto SecondaryMenu
+
+::RedMap::
+gg.searchNumber('0.58263474703;0.61197602749;0.69999998808', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber('0.58263474703', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(10000)
+gg.editAll('999', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.clearResults()
+gg.toast('Hack RED MAP Done')
+goto SecondaryMenu
+
+::LightBlueMap::
+gg.searchNumber('0.58263474703;0.61197602749;0.69999998808', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber('0.58263474703', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(10000)
+gg.editAll('-999', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.clearResults()
+gg.toast('Hack LIGHT BLUE MAP Done')
+goto SecondaryMenu
+
+::YellowMap::
+gg.searchNumber('0.58263474703;0.61197602749', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber('0.58263474703;0.61197602749', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(10000)
+gg.editAll('99', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.clearResults()
+gg.toast('Hack YELLOW MAP Done')
+goto SecondaryMenu
+
+::ThirdMenu::
+menu = gg.choice({'1.STRONG BROWN','2.WHITE YELLOW','3.STRONG YELLOW','4.LIGHT BROWN','5.WHITE SKIN','6.BLACK SKIN'},nil,'CHANGE SKIN COLOR [SET OF COLORS]')
+if menu == 1 then goto StrongBrown end
+if menu == 2 then goto WhiteYellow end
+if menu == 3 then goto StrongYellow end
+if menu == 4 then goto LightBrown end
+if menu == 5 then goto White end
+if menu == 6 then goto Black end
+if menu == nil then gg.alert('Thanks To Nivek')end goto start
+
+print('CancelScript')
+
+::StrongBrown::
+gg.searchNumber('3', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber('3', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(10000)
+gg.editAll('0', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.clearResults()
+gg.toast('Hack BODY STRONG BROWN Done')
+goto ThirdMenu
+
+::WhiteYellow::
+gg.searchNumber('3', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber('3', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(10000)
+gg.editAll('9', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.clearResults()
+gg.toast('Hack BODY WHITE YELLOW Done')
+goto ThirdMenu 
+
+::StrongYellow::
+gg.searchNumber('3', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber('3', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(10000)
+gg.editAll('9', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.clearResults()
+gg.toast('Hack BODY STRONG WHITE Done')
+goto ThirdMenu 
+
+::LightBrown::
+gg.searchNumber('3', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber('3', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(10000)
+gg.editAll('1', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.clearResults()
+gg.toast('Hack BODY LIGHT BROWN Done')
+goto ThirdMenu
+
+::White::
+gg.searchNumber('3', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber('3', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(10000)
+gg.editAll('99', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.clearResults()
+gg.toast('Hack BODY WHITE Done')
+goto ThirdMenu
+
+::Black::
+gg.searchNumber('3', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber('3', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(10000)
+gg.editAll('-9', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.clearResults()
+gg.toast('Hack BODY BLACK Done')
+goto ThirdMenu
+
+::FourthMenu::
+menu = gg.choice({'1.BLACK BODIES'},nil,'CHANGE SKIN COLOR [SET OF COLORS]')
+if menu == 1 then goto BlackBodies end
+if menu == nil then gg.alert('Thanks To Nivek')end goto start
+
+print('CancelScript')
+
+::BlackBodies::
+gg.searchNumber('1;3', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber('1', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(5500)
+gg.editAll('3', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.clearResults()
+gg.toast('Hack BLACK BODIES Done')
+goto FourthMenu 
+
+
+::bps::
+menu = gg.choice({'1.Resolve Freezing And Error {106}'},nil,'SELECT THIS WHEN APPEAR THE "GARENA" LOGO')
+if menu == 1 then goto Solution106 end
+if menu == nil then gg.alert('Happy Cheating')end goto start
+
+print('CancelScript')
+
+::Solution106::
+gg.searchNumber('0', gg.TYPE_AUTO, false, gg.SIGN_EQUAL, 0, -1)
+gg.alert('Error of Freezing And Error {106} Resolved.')
+gg.clearResults()
+goto bps
+
+::WallHack::
+gg.searchNumber('-5.9029581e21;-2.0291021e20;-2.0291021e20;1.0e-6', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber('1.0e-6', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(10000)
+gg.editAll('-1', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.clearResults()
+gg.toast('WALL HACK On')
+goto SmartCheater
+
+
+::BC::
+gg.clearResults()
+gg.searchNumber('1;5', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber('5', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(500)
+print('Replaced: ',gg.editAll('-5',
+gg.TYPE_FLOAT))
+gg.toast('Giant Only On Car Enable')
+goto SmartCheater
+
+::fc::
+gg.searchNumber('0.8', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1) gg.searchNumber('0.8', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1) gg.getResults(5000) gg.editAll('4.3', gg.TYPE_FLOAT) gg.clearResults(5000)gg.toast('The Flash of Crocodile On')goto SmartCheater
+
+::FLY::
+Cmenu =
+gg.choice({'Disable','Enable'},Lastt,'Happy Cheating') 
+if Cmenu == 1 then goto disfly end
+if Cmenu == 2 then goto enafly end
+if Cmenu == Last then  end goto SmartCheater
+
+::enafly::
+gg.clearResults()
+gg.searchNumber('1.1', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(1000)
+gg.editAll('40', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.toast('Fly Enabled')
+goto SmartCheater
+
+::disfly::
+gg.clearResults()
+gg.searchNumber('1,068,205,343', gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(1000)
+gg.editAll('1,111,205,343', gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.toast('Fly Disabled')
+goto SmartCheater
+
+::Underground::
+gg.searchNumber('4.3967567e-38;0.05000000075', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber('0.05000000075', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(10000)
+gg.editAll('2.3', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.clearResults()
+gg.toast('Underground Enabled')
+goto SmartCheater
+
+::bypass::
+gg.clearResults()
+gg.searchNumber('0', gg.TYPE_AUTO, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(10000)
+gg.editAll('0', gg.TYPE_AUTO, false, gg.SIGN_EQUAL, 0, -1)
+gg.toast('Bypass Succes')
+goto SmartCheater
+
+::GiantBody::
+gg.clearResults()
+gg.searchNumber('1;.05;.5', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber('1', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(10000)
+gg.editAll('0', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.toast('GiantBody On')
+goto SmartCheater
+
+::InstantDeath::
+gg.clearResults()
+gg.searchNumber('.08', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber('.08', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(10000)
+gg.editAll('99', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.toast('Suicide On')
+goto SmartCheater
+
+::Antenna::
+gg.searchNumber('0.65', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(3000)
+print('Replaced: ',gg.editAll('20', gg.TYPE_FLOAT))gg.toast('Antenna Female Aktif')
+goto SmartCheater
+
+
+::antenna2::
+gg.searchNumber('0.60', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0,-1)
+gg.getResults(3000)print('Replaced: ',gg.editAll('20', gg.TYPE_FLOAT))
+gg.toast('Antenna Male Aktif')
+goto SmartCheater
+
+::god::
+gg.searchNumber('0.15', gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0,-1)
+gg.getResults(3000)print('Replaced: ',gg.editAll('500', gg.TYPE_FLOAT))
+gg.toast('GodMode On')
+goto SmartCheater
+
+::on::
+gg.searchNumber('-0.1', gg.TYPE_FLOAT,false, gg.SIGN_EQUAL, 0,-1)
+gg.getResults(71)print('Replaced: ',gg.editAll('-99', gg.TYPE_FLOAT))
+gg.toast('No Fall On')
+goto SmartCheater
+
+::creator::
+print('Subscribe t.me/HighGamerz')
+gg.toast('Script by SmartCheat')
+print('Credit : SmartCheat')
+os.exit()
